e0f83c3cd052868c96bbed371704df78f8df7cfc
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-04-03 04:50:15 +0000

    Delete 2 test case(s)

--- Test Suites/Test Suite Purchase.ts
+++ Test Suites/Test Suite Purchase.ts
@@ -1,47 +1,27 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>Test Suite Purchase</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>33e7143f-d177-4686-90bd-c11e4bdb79c1</testSuiteGuid>
-   <testCaseLink>
-      <guid>b5bd2910-8c3c-4022-909d-c66fc36eb6d6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Purchase/Test Case 002 Add Item to Shopping Cart Renaming</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4e79b681-775c-4950-ace0-3c68c95da935</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>18390000-bce7-431c-b6b6-9e4a7b1e29a9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
+    <name>Test Suite Purchase</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>33e7143f-d177-4686-90bd-c11e4bdb79c1</testSuiteGuid>
+    <testCaseLink>
+        <guid>4e79b681-775c-4950-ace0-3c68c95da935</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>18390000-bce7-431c-b6b6-9e4a7b1e29a9</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
+    </testCaseLink>
 </TestSuiteEntity>
+
