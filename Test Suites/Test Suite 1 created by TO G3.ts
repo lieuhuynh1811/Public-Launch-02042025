7b7c5148a83d3c970aa954be324b621aad09e76b
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-04-14 09:59:18 +0000

    Create "Test Suite 1 created by TO G3" test suite

---
+++ Test Suites/Test Suite 1 created by TO G3.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Test Suite 1 created by TO G3</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>86792ad4-2f06-4084-9cde-d7e47e83c9ba</testSuiteGuid>
+    <testCaseLink>
+        <guid>cd20f39d-5ba3-42fa-94a9-49bbc3f7e7db</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Purchase/Test Case 004 Login with Invalid Password</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>ecc86146-2af7-44e1-8f04-7ca9e5db4fcf</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Purchase/Test Case 003 Checkout with Cash</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>aef632cd-c7f3-41ef-9465-c407817245d2</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Purchase/Test Case 002 Add Item to Shopping Cart</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
+
