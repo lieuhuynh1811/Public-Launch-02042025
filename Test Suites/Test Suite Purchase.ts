51c3f4ff2c784753c5ab21e56c645a9042bc85c9
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-05-14 02:58:15 +0000

    Delete "Test Case 001 API Response Time Validation" test case

--- Test Suites/Test Suite Purchase.ts
+++ Test Suites/Test Suite Purchase.ts
@@ -1,27 +1,26 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-    <name>Test Suite Purchase</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>3</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>33e7143f-d177-4686-90bd-c11e4bdb79c1</testSuiteGuid>
-    <testCaseLink>
-        <guid>4e79b681-775c-4950-ace0-3c68c95da935</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>18390000-bce7-431c-b6b6-9e4a7b1e29a9</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
-    </testCaseLink>
+   <name>Test Suite Purchase</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>33e7143f-d177-4686-90bd-c11e4bdb79c1</testSuiteGuid>
+   <testCaseLink>
+      <guid>4e79b681-775c-4950-ace0-3c68c95da935</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>18390000-bce7-431c-b6b6-9e4a7b1e29a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
-
--- Test Suites/healthcare-tests - TS_RegressionTest.ts
+++ Test Suites/healthcare-tests - TS_RegressionTest.ts
@@ -1,27 +1,26 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-    <name>healthcare-tests - TS_RegressionTest</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>0</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>false</rerunImmediately>
-    <testSuiteGuid>1286ccc6-2057-418d-87af-23d00cc6cd0e</testSuiteGuid>
-    <testCaseLink>
-        <guid>e4bf2157-13c0-4c5f-875c-fa60a7e7a2fa</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>955abe25-4029-43e2-b02f-57315c1ade49</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
-    </testCaseLink>
+   <name>healthcare-tests - TS_RegressionTest</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1286ccc6-2057-418d-87af-23d00cc6cd0e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e4bf2157-13c0-4c5f-875c-fa60a7e7a2fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utils/Test Case 004 Login with Invalid Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>955abe25-4029-43e2-b02f-57315c1ade49</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Utils/Test Case 003 Checkout with Cash</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
-
